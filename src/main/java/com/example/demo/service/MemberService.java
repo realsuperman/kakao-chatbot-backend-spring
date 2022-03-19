0bb55dda1f4c8895a753b74c373ee36f3bac7554
최성훈 <seonghun7304@naver.com>
2022-03-19 14:23:53 +0900

    스프링 테스트 케이스 및 간단한 레포지토리 설계

---
+++ src/main/java/com/example/demo/service/MemberService.java
@@ -0,0 +1,40 @@
+package com.example.demo.service;
+
+import com.example.demo.domain.Member;
+import com.example.demo.repository.MemberRepository;
+import com.example.demo.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /* 회원 가입 */
+    public Long join(Member member){
+        // 중복 회원 방지
+        checkUser(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    /* 전체 회원 조회 */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+
+    private void checkUser(Member member) {
+        memberRepository.findByName(member.getName())
+        .ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원");
+        });
+    }
+}
