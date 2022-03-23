34c2a0b2ddb59238bd11b935d4288ea022e95e80
최성훈 <seonghun7304@naver.com>
2022-03-23 16:51:42 +0900

    h2 console 연결 및 Member 컨트롤러 제작

--- src/main/java/com/example/demo/controller/MemberController.java
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -1,8 +1,14 @@
 package com.example.demo.controller;
 
+import com.example.demo.domain.Member;
 import  com.example.demo.service.MemberService;
         import org.springframework.beans.factory.annotation.Autowired;
         import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -12,4 +18,25 @@ public class MemberController {
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "/members/createMemberForm";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members",members);
+        return "members/membersList";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+        memberService.join(member);
+        return "redirect:/";
+    }
 }
+
--- src/main/java/com/example/demo/repository/MemoryMemberRepository.java
+++ src/main/java/com/example/demo/repository/MemoryMemberRepository.java
@@ -7,7 +7,7 @@ import java.util.*;
 
 public class MemoryMemberRepository implements MemberRepository {
     private static Map<Long,Member> store = new HashMap<>();
-    private static long sequence = 8L;
+    private static long sequence = 0L;
     @Override
     public Member save(Member member) {
         member.setId(++sequence);
